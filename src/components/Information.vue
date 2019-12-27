3bd740c75b939d136a73d997a842b6e64e951624
Whitebread <williampanebianco@gmail.com>
2019-12-27 18:42:21 -0500

    Complete initial cut
    * Clean up Informatio.vue
    * Clean up package.json

--- src/components/Information.vue
+++ src/components/Information.vue
@@ -1,13 +1,16 @@
 <template>
   <v-container fluid class="mx-0">
     <v-row align="center" justify="center" class="mx-2">
-      <h1 class="text-center">YOU MUST CONSTRUCT ADDITIONAL PYLONS</h1>
+      <h1 class="text-center">
+        BY THE LIGHT OF THE MOON
+      </h1>
     </v-row>
     <v-row align="center" justify="center" class="mx-2">
-      <p class="text-center mt-2 mb-0">
-        This domain contains quotes from classic blizzard entertainment games.
+      <p class="text-center text-justify mt-2 mb-0">
+        This domain contains quotes from classic Blizzard Entertainment games.
         Each game below leads to a user interface along with an API for said
-        game.
+        game containing memorable quotes. Click one of the games below for more
+        information.
       </p>
     </v-row>
     <v-row align="center" justify="center">
@@ -19,13 +22,13 @@
       >
         <a :href="image.href">
           <v-img :alt="image.alt" :src="image.src" class="game-icon" />
-          <p>{{ image.mouseOver }}</p>
         </a>
       </section>
     </v-row>
     <v-row align="center" justify="center" class="mx-2">
-      <p class="text-center mt-2 mb-0">
-        For questions or assistance, e-mail blizzardquotes@gmail.com
+      <p class="text-center text-justify">
+        No affiliation with Blizzard Entertainment. For questions, suggestions,
+        or assistance, e-mail blizzardquotes@gmail.com.
       </p>
     </v-row>
   </v-container>
@@ -36,7 +39,6 @@ export default {
   name: 'Information',
 
   data: () => ({
-    dimensions: '400',
     images: [
       {
         id: 'warcraft-3-quotes',
@@ -63,10 +65,10 @@ export default {
 
 <style>
 div.game-icon {
-  opacity: 0.85;
-  height: 400px;
-  width: 400px;
-  transition: opacity 0.15s;
+  opacity: 0.8;
+  height: 300px;
+  width: 300px;
+  transition: opacity 0.2s;
 }
 div.game-icon:hover {
   opacity: 1;
